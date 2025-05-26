7a53a8cfcd72d21769e24dd89bb56b602b513a35
caio-couto <cavalcantecaio.couto@gmail.com>
2025-05-26 06:22:24 -0300

    initial commit

---
+++ src/types/env.d.ts
@@ -0,0 +1,14 @@
+export enum NodeEnvOptions {
+	PRODUCTION = 0,
+	DEVELOPMENT = 1,
+	TEST = 2,
+}
+
+declare global {
+	namespace NodeJS {
+		interface ProcessEnv {
+			[key: string]: string | undefined;
+			NODE_ENV: keyof typeof NodeEnvOptions;
+		}
+	}
+}
---
+++ tests/unit/index.spec.ts
@@ -0,0 +1,5 @@
+describe("test", () => {
+	test("example", () => {
+		console.log("test");
+	});
+});
